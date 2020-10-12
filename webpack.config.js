abb3703d6ec514f119fe84ae2bd5588debbd17a8
Brian Sanchez <briansanchezizq@gmail.com>
2020-10-12 11:25:08 -0500

    Comentarios obre el archivo de configuracion webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+// Creamos un nuevo modulo que vamos a exportar con esta configuracion
+// Vamos a configurar cada unos de los elementos que necesitamos
 module.exports = {
+    // Iniciando por la entrada del proyecto
+  // Haciendo referencia al archivo principal
     entry: './src/index.js',
+    // En este output, es donde vamos a guardar los archivos resultantes cuando hagamos la configuracion
     output: {
         //path.resolve nos permite detectar el directorio donde nos encontramos
         //Su segundo argumento es el directorio donde se guadaran los archivos 
         path: path.resolve(__dirname, 'dist'),
+        // Filename nos pode un nombre al archivo compilado
         filename: 'bundle.js'
     },
+    // Este elemento resulve las extensiones que vamos a utilizar
     resolve: {
         extensions: ['.js', '.jsx']
     },
@@ -17,13 +24,17 @@ module.exports = {
             {
                 //Regla principal: identificacion de los archivos js/jsx
                 test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
+                // Exclusion de carpetas
+                exclude: /node_modules/,              
+                // Utilizamos el loader de babel instalado
                 use: {
                     loader: "babel-loader"
                 }
             },
             {
+                // Regla para trabajar con los archivos html
                 test: /\.html$/,
+                // Utilizamos el loader de babel instalado
                 use: [
                     {
                         loader: 'html-loader'
@@ -32,8 +43,10 @@ module.exports = {
             },
         ]
     },
+    // Se añaden los plugins que necesitamos
     plugins: [
         new HtmlWebpackPlugin({
+            //Donde esta ubicado el template que tenemos
             template: './public/index.html',
             filename: './index.html'
         })
