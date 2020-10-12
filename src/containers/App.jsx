614be55e756a15c2a1055d57341ea5725f0398a2
Brian Sanchez <briansanchezizq@gmail.com>
2020-10-12 13:56:25 -0500

    Reutilizamos el componente Categoires

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -11,7 +11,24 @@ const App = () => (
     <div className="app">
         <Header />
         <Search />
-        <Categories>
+        <Categories
+            title="Mi lista">
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
+        <Categories
+            title="Tendencias">
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>
+            </Carousel>
+        </Categories>
+        <Categories
+            title="Originales de Platzi Video">
             <Carousel>
                 <CarouselItem/>
                 <CarouselItem/>
